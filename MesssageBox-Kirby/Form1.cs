4f89f45d2a00faf5c2edc8d0736572a558eb66d6
Kirby Tutor:git config --blobal user.name Kirby <rockirbs@gmail.com>
2021-10-30 18:12:21 +0800

    First Commit

---
+++ MesssageBox-Kirby/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MesssageBox_Kirby
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi my name is Kirby. Since you've clicked the button, you're going to pass this sem!");
+        }
+    }
+}
